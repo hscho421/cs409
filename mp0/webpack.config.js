bee7780d34ae50f996d588927bc87563c6794a8c
Hyunseok Cho <55419130+hscho421@users.noreply.github.com>
2025-09-05 00:32:29 -0500

    added profile img and added watch for html

--- mp0/webpack.config.js
+++ mp0/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env, argv) => {
       historyApiFallback: true,
       port: 8080,
       open: true,
+      watchFiles: ['src/**/*.html']
     },
     entry: './src/index.js',
     module: {
